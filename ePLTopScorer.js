93c2ed8ea7fd03fdcd465b46c5dc7f88206c8805
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-14 11:47:24 +0100

    Refactored the file

--- ePLTopScorer.js
+++ ePLTopScorer.js
@@ -4,11 +4,39 @@ const cheerio = require('cheerio');
 const url =
   'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
 
-axios
-  .get(url)
-  .then(res => {
-    const $ = cheerio.load(res.data);
+const ePLTopScorers = (async () => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
     const statsTable = $('.statsTableContainer > tr');
-    console.log(statsTable);
-  })
-  .catch(err => console.log(err.message));
+    const topScorers = [];
+
+    statsTable.each(function() {
+      const rank = $(this)
+        .find('.rank > strong')
+        .text();
+      const name = $(this)
+        .find('.playerName > strong')
+        .text();
+      const nationality = $(this)
+        .find('.playerCountry')
+        .text();
+      const goals = $(this)
+        .find('.mainStat')
+        .text();
+
+      topScorers.push({
+        rank,
+        name,
+        nationality,
+        goals,
+      });
+    });
+
+    return topScorers;
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+
+ePLTopScorers.then(data => console.log(data));
