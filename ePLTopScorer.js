c50d3e7dbc63ac1cdf1ef9bbdbbd5c135213f207
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-14 11:18:44 +0100

    Loaded the html data with cheerio

--- ePLTopScorer.js
+++ ePLTopScorer.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const url =
   'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
 
 axios
   .get(url)
-  .then(res => console.log(res.data))
+  .then(res => {
+    const $ = cheerio.load(res.data);
+    const statsTable = $('.statsTableContainer > tr');
+    console.log(statsTable);
+  })
   .catch(err => console.log(err.message));
